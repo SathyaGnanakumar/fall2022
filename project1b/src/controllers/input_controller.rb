5e7f62a028aafde65c67d1fbaf4b02923f77378c
Sathya <sathya.gnanakumar@gmail.com>
2022-09-16 17:37:25 -0400

    Hello

--- project1b/src/controllers/input_controller.rb
+++ project1b/src/controllers/input_controller.rb
@@ -6,14 +6,61 @@ require_relative '../models/position'
 # Return nil on any error (validation error or file opening error)
 # If 5 valid ships added, return GameBoard; return nil otherwise
 def read_ships_file(path)
-    GameBoard.new 10, 10
+
+    #Format: (2,2), Right, 2
+
+    board = GameBoard.new 10, 10
+    num_ships_added = 0
+
+    read_file_lines(path) {|s|
+        if num_ships_added < 5 #We stop adding ships once we have added 5
+
+            modified = s.split #F(2,2), This is what the output after split is 
+                           #Right,  We need to remove ending commas on second entry
+                           #3
+
+            modified[1] = modified[1].delete(", ") #All the commas are gone now
+
+            #Now check the values
+
+            if modified[0] =~ /([0-9]+),([0-9]+)/ && modified[1] =~ /(Up|Down|Left|Right)/ && modified[2] =~ /[1-5]/
+                rowNum = $1.to_i #Extract row and col from modified[0]
+                colNum = $2.to_i
+
+                posToAdd = Position.new(rowNum, colNum)
+                shipToAdd = Ship.new(posToAdd, modified[1], modified[2].to_i)
+
+                if board.add_ship(shipToAdd) == true # Check if its possible
+                    num_ships_added += 1 #We added a ship so increment counter
+                end
+            end
+        end
+        
+    }
+
+    if num_ships_added != 5 #5 valid ships not added so we return nil
+        return nil
+    end
+
+    return board
 end
 
 
 # return Array of Position or nil
 # Returns nil on file open error
 def read_attacks_file(path)
-    [Position.new(1, 1)]
+    posArray = Array.new
+    idx = 0
+    read_file_lines(path) {|s|
+        if s =~ /^\((\d+),(\d+)\)$/
+            row = $1.to_i
+            col = $2.to_i
+            newPos = Position.new(row, col) #Create new position from values
+            posArray[idx] = newPos; #Add to array
+            idx += 1
+        end
+    }
+    return posArray
 end
 
 
--- project1b/src/models/game_board.rb
+++ project1b/src/models/game_board.rb
@@ -6,6 +6,8 @@ class GameBoard
     def initialize(max_row, max_column)
         @max_row = max_row
         @max_column = max_column
+        @successful_attacks = 0
+        @game_board = Array.new(max_row){Array.new(max_column)}
     end
 
     # adds a Ship object to the GameBoard
@@ -13,7 +15,9 @@ class GameBoard
     # Returns true on successfully added the ship, false otherwise
     # Note that Position pair starts from 1 to max_row/max_column
     def add_ship(ship)
-        true
+        #Boundary Checking
+        puts @game_board
+        return true
     end
 
     # return Boolean on whether attack was successful or not (hit a ship?)
@@ -21,27 +25,63 @@ class GameBoard
     def attack_pos(position)
         # check position
 
-        # update your grid
+        # If not inside the grid -> Return nil
+
+        if position.row < 1 || position.row > @max_row || position.column < 1 || position.column > @max_column
+            return nil
+        end
+
+        #update your grid
+        #Successful hit -> "B, A", Unsuccesful -> "-, A"
+
+        if(@game_board[position.row - 1][position.column - 1].eql? "B")
+            @successful_attacks += 1 #Increment counter variable 
+            @game_board[position.row - 1][position.column - 1] = "B, A"
+        else
+            @game_board[position.row - 1][position.column - 1] = "-, A"
+        end
+
 
         # return whether the attack was successful or not
-        true
+        # We have a hit when the pattern is "B, A"
+        if  @game_board[position.row - 1][position.column - 1] =~ /^B.*[^A]$/
+            return true
+        else
+            return false
+        end
+
     end
 
     # Number of successful attacks made by the "opponent" on this player GameBoard
     def num_successful_attacks
-        0
+        return @successful_attacks  
     end
 
     # returns Boolean
     # returns True if all the ships are sunk.
     # Return false if at least one ship hasn't sunk.
     def all_sunk?
-        true
+        for i in 0..@max_row - 1
+            for j in 0..@max_column - 1
+                if @game_board[i][j] =~ /^B.*[^A]$/ #B, A pattern means sunk so
+                                                    #anything besides this means not sunk
+                    return false  #Found a ship that is not sunk
+                end
+            end
+        end
+
+        return true #Every ship sunk so return true
     end
 
 
     # String representation of GameBoard (optional but recommended)
     def to_s
-        "STRING METHOD IS NOT IMPLEMENTED"
+        for i in 0..@max_row - 1
+            for j in 0..@max_column - 1
+                puts "| " + @game_board[i][j] + " "  
+            end
+
+            puts "|"
+        end
     end
 end
